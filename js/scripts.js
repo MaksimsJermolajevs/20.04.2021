cd826ebed8380295e4ad5b39f0f8ada0efcac0d0
MaksimsJermolajevs <MaksimsJermolajevs@github.com>
2021-04-20 17:03:50 +0300

    20.04.2021

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-console.log("Javascript is working")
+const POP_UP = document.getElementById('popUp');
+
+let produkti = [];
+
+document.getElementById('Prece').addEventListener('click',() => {
+    POP_UP.style.display = 'block';
+})
+
+document.getElementById('prece3').addEventListener('click',() => { 
+    POP_UP.style.display = 'none';
+
+    let prece = {Produkts: prece1.value, Skaits: prece2.value};
+
+    prece1.value= "";
+    prece2.value="";
+
+    produkti.push(prece);
+
+    render();
+})
+function render() {
+    let preces = document.getElementById('preces');
+
+    for(let i = 0; i < produkti.length; i++){
+        console.log(produkti[i])
+        let saraksts = `
+        <div class="saraksts">
+            <h3>Produkts: ${produkti[i].prece1}</h3>
+            <h4>Skaits: ${produkti[i].prece2}</h4>
+        </div>`;
+        preces.innerHTML = saraksts;
+}
+
+}
